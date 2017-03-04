03574bca1e8c9b1414b3c6c578650be429d7b102
JaiGatsby <jais334@gmail.com>
2017-03-04 15:38:25 +0800

    Chat activity and dependent files created

---
+++ app/src/main/java/com/example/user/lets/Chactivity.java
@@ -0,0 +1,13 @@
+package com.example.user.lets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Chactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chactivity);
+    }
+}
