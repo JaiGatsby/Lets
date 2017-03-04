0711fc449b83abed7f184a52f55844ae6264325e
akjadoon <asfandyarkjadoon@gmail.com>
2017-03-04 21:46:07 +0800

    Created Interface for Timetable

---
+++ app/src/main/java/com/example/user/lets/Views/Time_slot.java
@@ -0,0 +1,17 @@
+package com.example.user.lets.Views;
+
+/**
+ * Created by Asfandyar on 04-Mar-17.
+ */
+
+public class Time_slot {
+    private int startTime;
+    private int endTime;
+    private String days;
+
+    public Time_slot(int start, int end)
+    {
+
+    }
+
+}
